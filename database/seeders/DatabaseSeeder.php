32093e108b0da901dabcb80353749695cb6538cc
MaykeKerssens <93595867+MaykeKerssens@users.noreply.github.com>
2024-09-16 22:09:28 +0200

    Created TransitionSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             TrickSeeder::class,
+            TransitionSeeder::class,
         ]);
     }
 }
--- database/seeders/TrickSeeder.php
+++ database/seeders/TrickSeeder.php
@@ -13,45 +13,40 @@ class TrickSeeder extends Seeder
      */
     public function run(): void
     {
-        Trick::factory()
-        ->count(20)
-        ->create();
+        Trick::factory()->create([
+            'name' => 'Body spiral reverse grab attitude',
+            'is_mastered' => true,
+        ]);
 
-        // Create a trick with a specific name
         Trick::factory()->create([
             'name' => 'Handspring twisted grip',
-            'nickname' => 'Handspring TG',
             'description' => 'Handspring twisted grip is a pole trick that requires a lot of strength and flexibility. It is an advanced trick that is often performed by professional pole dancers.',
             'learned_at' => '2021-01-01',
             'is_mastered' => true,
             'difficulty' => 'hard',
             'energy_cost' => 'high',
-            'image_url' => 'https://example.com/handspring-twisted-grip.jpg',
-            'video_url' => 'https://example.com/handspring-twisted-grip.mp4',
         ]);
 
         Trick::factory()->create([
             'name' => 'Handspring cup grip',
-            'nickname' => 'Handspring CG',
             'description' => 'Handspring cup grip is a pole trick that requires a lot of strength and flexibility. It is an advanced trick that is often performed by professional pole dancers.',
             'learned_at' => '2021-01-01',
             'is_mastered' => false,
             'difficulty' => 'hard',
             'energy_cost' => 'high',
-            'image_url' => 'https://example.com/handspring-cup-grip.jpg',
-            'video_url' => 'https://example.com/handspring-cup-grip.mp4',
         ]);
 
         Trick::factory()->create([
             'name' => 'Handspring elbow grip',
-            'nickname' => 'Handspring EG',
             'description' => 'Handspring elbow grip is a pole trick that requires a lot of strength and flexibility. It is an advanced trick that is often performed by professional pole dancers.',
             'learned_at' => '2021-01-01',
             'is_mastered' => false,
             'difficulty' => 'expert',
             'energy_cost' => 'high',
-            'image_url' => 'https://example.com/handspring-elbow-grip.jpg',
-            'video_url' => 'https://example.com/handspring-elbow-grip.mp4',
         ]);
+
+        Trick::factory()
+        ->count(20)
+        ->create();
     }
 }
